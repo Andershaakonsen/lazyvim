43001c7bcde8febbd0fc80aec00ca0a09894de7a
Anders Håkonsen <andershakonsen@Anderss-MacBook-Pro.local>
2023-08-17 09:01:45 +0200

    MarkdownPreview and Csharp lsp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,5 +14,5 @@ return {
   },
   { "folke/zen-mode.nvim", lazy = true },
   { "christoomey/vim-tmux-navigator" },
-  { "iamcco/markdown-preview.nvim", lazy = true },
+  { "iamcco/markdown-preview.nvim" },
 }
